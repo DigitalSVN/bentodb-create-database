8d506c689e0f35aea99e5edd296ddabac53eaebd
Jon Milsom <jon@altstudio.co.uk>
2023-05-08 19:53:01 +0100

    Hoping GHA will obscure password

--- .github/workflows/test.php
+++ .github/workflows/test.php
@@ -1,9 +1,9 @@
 <?php
-$db_user = getenv('DB_USER');
-$db_pass = getenv('DB_PASS');
 $db_host = getenv('DB_HOST');
 $db_port = getenv('DB_PORT');
 $db_name = getenv('DB_NAME');
+$db_username = getenv('DB_USERNAME');
+$db_password = getenv('DB_PASSWORD');
 
 $dsn = sprintf(
     'mysql:host=%s;port=%s;dbname=%s',
